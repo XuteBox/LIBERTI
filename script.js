f7f0bb80d691ac36a46f05285fe4dd97f7493056
XuteBox <luckyxute@gmail.com>
2025-09-22 21:43:46 +0200

    Init Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const nav = document.getElementById('navigation')
+let lastScrollY = window.scrollY
+
+document.addEventListener('scroll', () => {
+    if (lastScrollY < window.scrollY) {
+        nav.classList.add("--hidden")
+    }
+    else {
+        nav.classList.remove("--hidden")
+    }
+    lastScrollY = window.scrollY
+})
+
+
+/* const cursor = document.querySelector('.custom-cursor.site-wide')
+const pointer = document.querySelector('.site-wide .pointer')
+
+document.body.addEventListener('mouseenter', () => {
+    cursor.style.display = 'block';
+});
+
+document.body.addEventListener('mouseleave', () => {
+    cursor.style.display = 'none';
+});
+
+document.addEventListener('mousemove', (e) => {
+    const w = cursor.clientWidth;
+    const h = cursor.clientHeight;
+    cursor.style.transform = `translate(${e.clientX - w/2}px, ${e.clientY - h/2}px)`
+})
+
+document.addEventListener('mousedown', (e) => {
+    pointer.classList.add('active')
+})
+
+document.addEventListener('mouseup', (e) => {
+    pointer.classList.remove('active')
+}) */
